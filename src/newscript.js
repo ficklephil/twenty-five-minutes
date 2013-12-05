c3a61ad7abb2882fe80ce4bf49549a33dda5cf2b
Philip <ficklephil@hotmail.com>
2013-12-05 23:35:03 +0000

    JavaSCript

--- src/newscript.js
+++ src/newscript.js
@@ -2,12 +2,32 @@ var TWENTY_FIVE_MINUTES=1500000;
 var countdown=TWENTY_FIVE_MINUTES;
 var date;
 var timer;
+var timerRunning = false;
+var countdownCompleteTime;
 
-var startTimer = function(){
-    timer = setInterval(function(){
-        intervalComplete();
-    }, 1000);
+function Timer(callback,delay){
+
+    var timerId, start, remaining = delay;
+
+    this.pause = function(){
+        window.clearTimeout(timerId);
+        remaining -= new Date() - start;
+    };
+
+    this.resume = function(){
+        start = new Date();
+        timerId = window.setTimeout(callback,remaining);
+    };
+
+    this.resume();
 }
+
+
+//var startTimer = function(){
+//    timer = setInterval(function(){
+//        intervalComplete();
+//    }, 1000);
+//}
 var intervalComplete = function(){
     console.log('INTERVAL COMPLETE');
     if(0 < countdown){
@@ -33,9 +53,48 @@ var timerComplete = function(){
     console.log($('.task').val());
 }
 
+var start = function(){
+    console.log('start');
+
+    //if it's running that is the timer
+    console.log(timer);
+
+    //do not start if it's running
+
+    if(!timerRunning)
+    {
+        timer = setInterval(function(){
+            intervalComplete();
+        }, 1000);
+
+        timerRunning = true;
+    }
+}
+
+//var start = new Timer(function(){
+//    intervalComplete();
+//},1000)
+
+var pause = function(){
+    clearInterval(timer);
+    timerRunning = false;
+}
+
+var reset = function(){
+    countdown = countdownCompleteTime;
+    $('.timer-display').text(milliSecondsToTimeFormat(countdown));
+    clearInterval(timer);
+    timerRunning = false;
+}
+
 var setTimeButtonClick = function(time){
-    countdown=time;
-    startTimer();
+
+    console.log('set time ' + time);
+    countdown = time;
+    countdownCompleteTime = time;
+    clearInterval(timer);
+    timerRunning = false;
+    $('.timer-display').text(milliSecondsToTimeFormat(countdown));
 }
 
 var milliSecondsToTimeFormat = function(miliseconds){
